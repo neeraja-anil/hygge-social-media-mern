831360673d899c956ca16374bef8e175a275342f
neeraja <neerajaanilkumar001@gmail.com>
2023-05-25 20:14:24 +0530

    user model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        //required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+    picturePath: {
+        type: String,
+        default: ""
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    location: String,
+    occupation: String,
+    viewedProfiles: Number,
+    impressions: Number,
+}, {
+    timestamps: true
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+//@desc  method to hash password of a newly created user before save
+userSchema.pre('save', async function (next) {
+
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
