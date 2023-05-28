dd04050a56e860e34b758c05bdfa5175a62aa6b2
neeraja <neerajaanilkumar001@gmail.com>
2023-05-28 22:24:34 +0530

    react router dom setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      hello!
+      <Header />
+      <main className='py-3'>
+        <Routes>
+          <Route exact path='/' element={<HomeScreen />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
