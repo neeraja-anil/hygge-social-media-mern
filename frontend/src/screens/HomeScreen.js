7551cd6d7c628cda556add640a7d1cd12d11bc68
neeraja <neerajaanilkumar001@gmail.com>
2023-06-10 23:57:31 +0530

    screens and components

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeScreen = () => {
+    return (
+        <>
+            Homescreen
+        </>
+    )
+}
+
+export default HomeScreen
---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginScreen = () => {
+    return (
+        <div>
+            LoginScreen
+        </div>
+    )
+}
+
+export default LoginScreen
