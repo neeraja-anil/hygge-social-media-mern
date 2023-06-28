1a09459a368a422f8d4dbbbc39cabb14b0abc923
neeraja <neerajaanilkumar001@gmail.com>
2023-06-28 19:31:25 +0530

    headers autherization token set in apiSlice

--- frontend/src/redux/apiSlice.js
+++ frontend/src/redux/apiSlice.js
@@ -1,7 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001' }),
-    //tagTypes: ['user'],
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5001',
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().auth.token ? getState().auth.token : null //the token is stored in the "auth" state slice
+            if (!headers.has("Authorization") && token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    tagTypes: ['Post', 'User'],
     endpoints: (builder) => ({})
 })
