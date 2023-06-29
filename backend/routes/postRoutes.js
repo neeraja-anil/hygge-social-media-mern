63e42f1a452d8c6c42e34207c532582ee4f0bde0
neeraja <neerajaanilkumar001@gmail.com>
2023-06-29 20:30:39 +0530

    post like funstionality added

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { protect } from '../middlewares/authMiddleware.js'
-import { createNewPost, getPostById, deletePost, getAllPosts } from '../controllers/postController.js'
+import { createNewPost, getPostById, deletePost, getAllPosts, likePost } from '../controllers/postController.js'
 import { upload } from '../utils/multer.js'
 
 const router = express.Router()
 
 router.route('/create').post(protect, upload.single('post'), createNewPost)
 router.route('/:id').get(protect, getPostById).delete(protect, deletePost)
+router.route('/:id/like').put(protect, likePost)
 router.route('/').get(protect, getAllPosts)
 
 export default router
