e41ae0c4dc9e3dbf9fa3cad28d95e1b5ffa91ab4
neeraja anil <neerajaanilkumar001@gmail.com>
2023-11-21 23:54:58 +0530

    .

--- frontend/src/redux/apiSlice.js
+++ frontend/src/redux/apiSlice.js
@@ -1,16 +1,16 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5001',
-        prepareHeaders: (headers, { getState }) => {
-            const token = getState().auth.token ? getState().auth.token : null //the token is stored in the "auth" state slice
-            if (!headers.has("Authorization") && token) {
-                headers.set("Authorization", `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }),
-    tagTypes: ['Post', 'User', 'Messages', 'Conversations', 'Notification'],
-    endpoints: (builder) => ({})
-})
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://hygge-inhy.onrender.com",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token ? getState().auth.token : null; //the token is stored in the "auth" state slice
+      if (!headers.has("Authorization") && token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Post", "User", "Messages", "Conversations", "Notification"],
+  endpoints: (builder) => ({}),
+});
